fd45d35189e849fa9adf3ec635b4741f3a03667c
inuex35 <inuex3@yahoo.co.jp>
2026-02-22 21:16:02 +0900

    general sfm factor

--- gtsam/slam/GeneralSFMFactor.h
+++ gtsam/slam/GeneralSFMFactor.h
@@ -20,11 +20,11 @@
 
 #pragma once
 
+#include <gtsam/geometry/MeasurementTraits.h>
 #include <gtsam/geometry/PinholeCamera.h>
 #include <gtsam/geometry/Point2.h>
 #include <gtsam/geometry/Point3.h>
 #include <gtsam/geometry/Pose3.h>
-#include <gtsam/geometry/Unit3.h>
 #include <gtsam/nonlinear/NonlinearFactor.h>
 #include <gtsam/nonlinear/NoiseModelFactorN.h>
 #include <gtsam/linear/BinaryJacobianFactor.h>
@@ -32,7 +32,6 @@
 #include <gtsam/base/concepts.h>
 #include <gtsam/base/Manifold.h>
 #include <gtsam/base/Matrix.h>
-#include <gtsam/base/OptionalJacobian.h>
 #include <gtsam/base/SymmetricBlockMatrix.h>
 #include <gtsam/base/types.h>
 #include <gtsam/base/Testable.h>
@@ -44,7 +43,6 @@
 #endif
 #include <iostream>
 #include <string>
-#include <optional>
 #include <type_traits>
 
 namespace boost {
@@ -54,36 +52,6 @@ class access;
 } /* namespace boost */
 
 namespace gtsam {
-
-namespace internal {
-
-template <class MEASUREMENT>
-struct MeasurementErrorHelper {
-  static constexpr int Dim = traits<MEASUREMENT>::dimension;
-  using VectorType = Eigen::Matrix<double, Dim, 1>;
-  using MatrixType = Eigen::Matrix<double, Dim, Dim>;
-
-  static VectorType Evaluate(const MEASUREMENT& measured,
-                             const MEASUREMENT& predicted,
-                             OptionalJacobian<Dim, Dim> H_predicted) {
-    if (H_predicted) *H_predicted = MatrixType::Identity();
-    auto local = traits<MEASUREMENT>::Local(measured, predicted);
-    return VectorType(local);
-  }
-};
-
-template <>
-struct MeasurementErrorHelper<Unit3> {
-  static constexpr int Dim = traits<Unit3>::dimension;
-  using VectorType = Eigen::Matrix<double, Dim, 1>;
-
-  static VectorType Evaluate(const Unit3& measured, const Unit3& predicted,
-                             OptionalJacobian<Dim, Dim> H_predicted) {
-    return measured.errorVector(predicted, {}, H_predicted);
-  }
-};
-
-}  // namespace internal
 
 /**
  * Non-linear factor for a constraint derived from a 2D measurement.
@@ -176,16 +144,12 @@ public:
           camera.project2(point, H1 ? &Dcamera : nullptr, H2 ? &Dlandmark : nullptr);
 
       MeasurementJacobian localJacobianStorage;
-      OptionalJacobian<ZDim, ZDim> localJac;
-      if (H1 || H2)
-        localJac = OptionalJacobian<ZDim, ZDim>(&localJacobianStorage);
+      OptionalJacobian<ZDim, ZDim> localJac((H1 || H2) ? &localJacobianStorage : nullptr);
       Vector error = Vector(internal::MeasurementErrorHelper<Measurement>::Evaluate(
           measured_, predicted, localJac));
 
-      if (H1 && localJac) *H1 = localJacobianStorage * Dcamera;
-      if (H2 && localJac) *H2 = localJacobianStorage * Dlandmark;
-      if (H1 && !localJac) *H1 = Dcamera;
-      if (H2 && !localJac) *H2 = Dlandmark;
+      if (H1) *H1 = localJac ? localJacobianStorage * Dcamera : Dcamera;
+      if (H2) *H2 = localJac ? localJacobianStorage * Dlandmark : Dlandmark;
 
       return error;
     } catch (CheiralityException& e [[maybe_unused]]) {
